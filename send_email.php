a9be2a58c5c430dd033931e6822fc7c2a47f9005
Dhara Castilho <dhara.castilho@gmail.com>
2024-04-30 20:47:27 -0300

    Alteração PHP - script novo

--- send_email.php
+++ send_email.php
@@ -1,7 +1,5 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 require 'vendor/autoload.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -19,27 +17,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Configuração do servidor SMTP
         $mail->isSMTP();
         $mail->Host = 'smtp.hostinger.com';
+        $mail->Port = 587;
         $mail->SMTPAuth = true;
         $mail->Username = 'contato@dharacastilho.com';
         $mail->Password = 'Dh@riTch*#8';
-        $mail->SMTPSecure = 'tls'; 
-        $mail->Port = 587; 
 
         // Configurações adicionais
         $mail->setFrom('contato@dharacastilho.com', 'Dhara Castilho');
-        $mail->addReplyTo('contato@dharacastilho.com', 'Dhara Castilho');
         $mail->addAddress('contato@dharacastilho.com', 'Dhara Castilho'); // Endereço de e-mail do destinatário
-        $mail->Subject = $assunto;
-        $mail->Body = "Nome: $nome\nEmail: $email\n\nMensagem:\n$mensagem";
+        $mail->Subject = 'PHPMailer contact form';
+        $mail->isHTML(false);
+        $mail->Body = "Nome: $nome\nEmail: $email\n\nAssunto: $assunto\n\nMensagem:\n$mensagem";
 
         // Envia o e-mail
-        $mail->send();
-
-        http_response_code(200);
-        echo json_encode(array("message" => "Email enviado com sucesso!"));
+        if ($mail->send()) {
+            http_response_code(200);
+            echo json_encode(array("message" => "Email enviado com sucesso!"));
+        } else {
+            http_response_code(500);
+            echo json_encode(array("message" => "Erro ao enviar email: " . $mail->ErrorInfo));
+        }
     } catch (Exception $e) {
         http_response_code(500);
-        echo json_encode(array("message" => "Erro ao enviar email: " . $mail->ErrorInfo));
+        echo json_encode(array("message" => "Erro ao enviar email: " . $e->getMessage()));
     }
 } else {
     http_response_code(405);
